c5400fe20bf3775ae210758eb95c1c8c4a16b818
Олександр Маркович <markoleks08@gmail.com>
2025-01-05 20:16:23 +0200

    PACKAGE json 3!!!

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const App = () => {
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+        axios.get('http://localhost:5000/api/data')
+            .then(response => setData(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
+    return <div>{data ? data.message : 'Loading...'}</div>;
+};
+
+export default App;
