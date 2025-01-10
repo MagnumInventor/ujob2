6ecb0ec7147f13ae7fa0e60e1a0e306d78b4cae3
Олександр Маркович <markoleks08@gmail.com>
2025-01-10 15:48:44 +0200

    local porrt SET to 3003

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,6 @@ app.get('*', (req, res) => {
 });
 
 // Запуск сервера
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(3003, () => {
+    console.log('Server is running on port 3003');
 });
