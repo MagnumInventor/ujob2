72ebd375e36eca845b5aec52fb79e6bc82c39fb1
Олександр Маркович <markoleks08@gmail.com>
2025-01-11 18:00:26 +0200

    export default App;

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "src/**/*.{html,js,jsx,ts,tsx}",
+    "frontend/src/pages/*.{html,js,jsx}",
+    "frontend/src/components/landing/*.{html,js,jsx}",
     "frontend/src/App.jsx",
+    "D:/Projects/ujob2/frontend/src/globals.css",
     "frontend/src/globals.css"
   ],
   theme: {
