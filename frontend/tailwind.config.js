025e2b6ea9b78d6cd44ed5dac0194930cc061e7a
Олександр Маркович <markoleks08@gmail.com>
2025-01-11 20:26:31 +0200

    f gf

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "src/**/*.{html,js,jsx,ts,tsx}",
-    "frontend/src/pages/*.{html,js,jsx}",
-    "frontend/src/components/landing/*.{html,js,jsx}",
-    "frontend/src/App.jsx",
-    "D:/Projects/ujob2/frontend/src/globals.css",
-    "frontend/src/globals.css"
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./frontend/src/pages/*.{html,js,jsx}",
+    "./frontend/src/components/landing/*.{html,js,jsx}",
+    "./frontend/src/App.jsx",
+    "./frontend/src/globals.css",
+    "D:/Projects/ujob2/frontend/src/globals.css"
   ],
   theme: {
     extend: {},
