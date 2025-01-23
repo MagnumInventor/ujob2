85cf6c433aae3867d20f0151e294220b087b2a98
Олександр Маркович <markoleks08@gmail.com>
2025-01-23 22:28:02 +0200

    ірщке зфееукт

--- frontend/src/components/user/worker/ProjectContainer.jsx
+++ frontend/src/components/user/worker/ProjectContainer.jsx
@@ -1,57 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-const Team = ({ team = [] }) => (
-  <div className="team">
-    <h3>Учасники проекту:</h3>
-    {team.map((member, index) => (
-      <div key={index} className="team-member">
-        {member.name} <img src={member.image} alt="member" />
-      </div>
-    ))}
-  </div>
-);
-
-const Vacancies = ({ vacancies = [] }) => (
-  <div className="card-body">
-    {vacancies.map((role, index) => (
-      <div key={index} className="role">
-        {role}
-      </div>
-    ))}
-  </div>
-);
-
-const ProjectCard = ({ project }) => {
-  const [vacanciesActive, setVacanciesActive] = useState(false);
-
-  const toggleVacancies = () => {
-    setVacanciesActive(!vacanciesActive);
-  };
-
-  return (
-    <div className="card">
-      <div className="card-header">
-        <h2>{project.name}</h2>
-      </div>
-      <div className="vacancy-info">
-        <h3>Опис:</h3>
-        <p>{project.description}</p>
-      </div>
-      <button
-        className={`open-vacancy-button ${vacanciesActive ? 'active' : ''}`}
-        onClick={toggleVacancies}
-      >
-        <span className="check-left">&#11013;</span>
-        ВІЛЬНІ ПОСАДИ
-        <span className="check-right">&#11013;</span>
-      </button>
-      {vacanciesActive && <Vacancies vacancies={project.vacancies} />}
-      <Team team={project.team} />
-      <button className="view-button">ДЕТАЛЬНІШЕ</button>
-    </div>
-  );
-};
-
 const ProjectsContainer = () => {
   const [projects, setProjects] = useState([]);
 
@@ -70,7 +18,10 @@ const ProjectsContainer = () => {
   return (
     <div id="projects-container">
       {projects.map((project, index) => (
-        <ProjectCard key={index} project={project} />
+        <div key={index}>
+          <h2>{project.name}</h2>
+          <p>{project.description}</p>
+        </div>
       ))}
     </div>
   );
