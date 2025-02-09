824c22e7f33e92b202e1191fe8304f63a7e44703
Олександр Маркович <markoleks08@gmail.com>
2025-02-09 23:56:08 +0200

    UP for 09.02

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero() {
         </h1>
         <h2 className={styles.subheadline}>ujob.com</h2>
         <p className="paragraph">Розширення можливостей нового покоління інноваторів і компаній</p>
-        <a href="#" className="btn btn-primary">Досліджуйте можливості</a>
+        <a href="/services" className="btn btn-primary">Досліджуйте можливості</a>
       </div>
       <div className={styles.decoration}>
         <div className={styles.circle}></div>
